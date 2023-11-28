090304137b2b91f46c912d68eda244539a5ff238
Sabbat10 <sabbatlumpatshia99@gmail.com>
2023-11-28 09:02:16 +0100

    add the state

--- src/Components/App/App.jsx
+++ src/Components/App/App.jsx
@@ -5,15 +5,29 @@ import './App.css'
 import Eleve from '../Eleves/Eleve'
 
 function App() {
+  const state = {
+    eleve : [
+      {nom : 'Sabbat Lumpatshia', moyenne: 20},
+      {nom : 'Meghan Kasala', moyenne: 15}
+    ]
+  }
   return (
     <div>
       <h1>Bienvenue dans la classe de Terre</h1>
+
       <div className=''>
-        <Eleve nom="Sabbat Lumpatshia" moyenne="20">"Aller toujour plus loin"</Eleve>
-        <Eleve nom="Meghan Kasala" moyenne="15"/>
-        <Eleve nom="Alliance Tshidanyi" moyenne="10"/>
+        {/* recuperation des information grâce au state */}
+        <Eleve 
+            nom={state.eleve[0].nom} moyenne={state.eleve[0].moyenne}>"Aller toujour plus loin"
+        </Eleve>
+
+        <Eleve 
+            nom={state.eleve[1].nom} moyenne={state.eleve[1].moyenne}>
+        </Eleve>
+      
       </div>
     </div>
+
    )
 }
 export default App
